d1df005e0a6b5b45f4f9cb29332cdb7bf4f05646
JuanFra <juanfraa032@gmail.com>
2023-08-02 19:22:21 -0300

    fix

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -177,7 +177,7 @@ botonLimpiarCarrito.onclick = () => {
             vaciar.innerHTML = `
                 <p class="estilo" id="vacio">No hay productos en su carrito por el momento</p>
                 `
-            window.location.href = "../Pages/tarjeta.html";
+            window.location.href = "Pages/tarjeta.html";
         }, 1600);
     } else {
         Swal.fire({
